5c4671486889fa8b52681afc5595151db0dfb440
Maximus Brutalis <dmitroid1sk@gmail.com>
2025-02-26 23:17:09 -0700

    user profiles introduced

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    # defines how a profile is printed 
+    def __str__(self):
+        return f'{self.user.username} Profile'
